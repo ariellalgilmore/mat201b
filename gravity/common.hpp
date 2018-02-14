274a245ae2f62dd12461e8a3979b1002430610d3
Ariella Gilmore <ariellalgilmore@gmail.com>
2018-02-13 16:02:07 -0800

    Create common.hpp

---
+++ gravity/common.hpp
@@ -0,0 +1,17 @@
+using namespace al;
+
+#ifndef _COMMON_STUFF_
+#define _COMMON_STUFF_
+
+#define PORT (54537)
+#define CHUNK_SIZE (1400)
+
+// #define particleCount (50)
+
+
+struct State{
+	Vec3f pos[1000];
+	Pose pose;
+};
+
+ #endif
